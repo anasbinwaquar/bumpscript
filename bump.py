a524a1345b1350c1a17780f7e0ce8903bef97067
Anas Bin Waquar <anasbinwaquar@yahoo.com>
2020-07-23 18:44:34 +0500

    v

--- bump.py
+++ bump.py
@@ -38,7 +38,7 @@ def Bump(driver):
 			break
 	return
 
-url = 'https://backpack.tf/classifieds?page=1"&steamid=76561198086548576'
+url = "https://steamcommunity.com/openid/login?openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.mode=checkid_setup&openid.return_to=http%3A%2F%2Fbackpack.tf%2Flogin&openid.realm=http%3A%2F%2Fbackpack.tf&openid.ns.sreg=http%3A%2F%2Fopenid.net%2Fextensions%2Fsreg%2F1.1&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select"
 chrome_options = Options()
 # chrome_options.add_argument("headless")
 chrome_options.add_argument("--no-sandbox")
@@ -46,33 +46,33 @@ chrome_options.add_argument("--user-data-dir=./chrome-data")
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(url)
 
-# username=getpass.getpass("Enter username:")
-# password=getpass.getpass("Enter pass:")
-# # Find username textbox
-# driver.get_screenshot_as_file("log.png")
-# username_textbox = driver.find_element_by_id("steamAccountName")
-# username_textbox.send_keys(username)
+username=getpass.getpass("Enter username:")
+password=getpass.getpass("Enter pass:")
+# Find username textbox
+driver.get_screenshot_as_file("log.png")
+username_textbox = driver.find_element_by_id("steamAccountName")
+username_textbox.send_keys(username)
 
-# # Find Password textbox
-# password_textbox = driver.find_element_by_id("steamPassword")
-# password_textbox.send_keys(password)
+# Find Password textbox
+password_textbox = driver.find_element_by_id("steamPassword")
+password_textbox.send_keys(password)
 
-# # Hit login button
-# login_button = driver.find_element_by_id("imageLogin")
-# login_button.submit()
+# Hit login button
+login_button = driver.find_element_by_id("imageLogin")
+login_button.submit()
 
-# time.sleep(1)
+time.sleep(1)
 
-# # Input 2FA Code
-# twofactor = getpass.getpass("Enter code: ")
-# twofactor_textbox = driver.find_element_by_id("twofactorcode_entry")
-# twofactor_textbox.send_keys(twofactor)
+# Input 2FA Code
+twofactor = getpass.getpass("Enter code: ")
+twofactor_textbox = driver.find_element_by_id("twofactorcode_entry")
+twofactor_textbox.send_keys(twofactor)
 
-# twofactor_button = driver.find_element_by_xpath("//*[@id='login_twofactorauth_buttonset_entercode']/div[1]")
-# twofactor_button.click()
-# time.sleep(10)
-# driver.get('https://backpack.tf/classifieds?steamid=76561198086548576')
-# time.sleep(5)
+twofactor_button = driver.find_element_by_xpath("//*[@id='login_twofactorauth_buttonset_entercode']/div[1]")
+twofactor_button.click()
+time.sleep(10)
+driver.get('https://backpack.tf/classifieds?steamid=76561198086548576')
+time.sleep(5)
 Bump(driver)
 print("Bumped")
 while(True):
